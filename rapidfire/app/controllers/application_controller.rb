ce0450220d1eb7448f8bf3fc2f71765ff5ec16b2
David Barrett <davidbarrett72@gmail.com>
2014-04-07 18:32:37 +0100

    working version

---
+++ rapidfire/app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  #protect_from_forgery with: :exception  
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) || User.new
+    end
+
+    def can_administer?
+      true #current_user.try(:admin?)
+    end
+
+  
+end
